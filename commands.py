202fae766aee47dd7b45d7443cbd20e92871e884
yezy <yezileliilomo@hotmail.com>
2017-09-23 19:48:15 +0300

    Adding create_db, drop, drop_db and migrate commands

--- commands.py
+++ commands.py
@@ -1,15 +1,21 @@
 import click, os, sys
 sys.path.append(os.getcwd())
-from raw_db import *
+try:
+  from raw_db import *
+except Exception as e:
+    print('\033[1m'+'\033[91m'+"Your are not in the right directory!."+'\033[0m')
+    print("Please make sure you are in '.../database' directory and there is 'raw_db.py' in it.\n")
+
 
 @click.group()
 def cli():
   pass
 
-@click.command('create', short_help='This is a command for creating a database with argument as class name from which a db is created')
+@click.command('migrate', short_help='This is a command for migrating database schema(defined as python classes) with argument as class name from which a database table is created')
 @click.argument('args', nargs=-1)
-def create(args):
-   if len(args)==0:
+@click.option('--all', 'flg', flag_value='all', default=True)
+def migrate(args,flg):
+   if len(args)==0 and flg=='all':
       try:
         run()
         print('\033[1m'+'\033[93m'+ "All table created successfully"+'\033[0m')
@@ -17,10 +23,9 @@ def create(args):
         print('\033[1m'+'\033[91m'+"there was a problem in creating database"+'\033[0m')
         if 'run' not in globals():
            print("Method 'run' is not defined in your 'raw_db' file ")
-        else:  
+        else:
            print(e)
    else:
-     
      for class_name in args:
        try:
          globals()[class_name]().create()
@@ -32,5 +37,54 @@ def create(args):
           else:
              print(e)
 
-          
-cli.add_command(create)
+@click.command('drop', short_help='This is a command for droping a database table with argument as table name for droping and option --all incase you want to drop all tables')
+@click.option('--all', 'flg', flag_value='all')
+@click.argument('tables', nargs=-1)
+def drop(tables,flg):
+     if len(tables)==0 and flg != 'all':
+       print('\033[1m'+'\033[91m'+"Specify table(s) to drop!."+'\033[0m')
+       return
+     if flg=='all':
+        if len(db.db__tables__)==0:
+          print('\033[1m'+'\033[91m'+"No table to drop!."+'\033[0m')
+          return
+        db.db__tables__.reverse()
+        tables=db.db__tables__
+
+     for table in tables:
+       try:
+         db.drop(table)
+         print('\033[1m'+'\033[93m'+"'"+table+"' table dropped successfully"+'\033[0m')
+       except Exception as e:
+          print('\033[1m'+'\033[91m'+"failed to drop '"+table+"' table"+'\033[0m')
+          if table not in db.db__tables__:
+             print("'"+table+"' is not defined in your database")
+          else:
+             print(e)
+
+
+@click.command('create_db', short_help='This is a command for creating a database with argument as db name to create')
+@click.argument('db_name')
+def create_db(db_name):
+       try:
+         db.create_db(db_name)
+         print('\033[1m'+'\033[93m'+"'"+db_name+"' database created successfully"+'\033[0m')
+       except Exception as e:
+          print('\033[1m'+'\033[91m'+"failed to create '"+table+"' database"+'\033[0m')
+          print(e)
+
+@click.command('drop_db', short_help='This is a command for droping a database with argument as database name to drop')
+@click.argument('db_name', nargs=-1)
+def drop_db(db_name):
+     for name in db_name:
+       try:
+         db.drop_db(name)
+         print('\033[1m'+'\033[93m'+"'"+name+"' database dropped successfully"+'\033[0m')
+       except Exception as e:
+          print('\033[1m'+'\033[91m'+"failed to drop '"+name+"' database"+'\033[0m')
+          print(e)
+
+cli.add_command(migrate)
+cli.add_command(drop)
+cli.add_command(create_db)
+cli.add_command(drop_db)
